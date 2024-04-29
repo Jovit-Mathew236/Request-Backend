1115fae76658b1d4d9a946491adf3ee054d67270
Jovit-Mathew236 <jovitmathew236@gmail.com>
2024-04-29 12:17:03 +0530

    Update role controller, module, and service to use Prisma for database operations

--- src/role/role.module.ts
+++ src/role/role.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RoleService } from './role.service';
 import { RoleController } from './role.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [RoleController],
   providers: [RoleService],
 })
--- src/role/role.service.ts
+++ src/role/role.service.ts
@@ -1,26 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class RoleService {
+  constructor(private readonly prisma: PrismaService) {}
   create(createRoleDto: CreateRoleDto) {
-    return 'This action adds a new role';
+    return this.prisma.roles.create({
+      data: createRoleDto,
+    });
   }
 
   findAll() {
-    return `This action returns all role`;
+    return this.prisma.roles.findMany();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} role`;
+    return this.prisma.roles.findUnique({
+      where: { id },
+    });
   }
 
   update(id: number, updateRoleDto: UpdateRoleDto) {
-    return `This action updates a #${id} role`;
+    return this.prisma.roles.update({
+      where: { id },
+      data: updateRoleDto,
+    });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} role`;
+    return this.prisma.roles.delete({
+      where: { id },
+    });
   }
 }
